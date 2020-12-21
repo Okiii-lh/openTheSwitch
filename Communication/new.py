87727d11c220fa54dfa836fc092aed5eb00d3893
liuhe_tech <13132515202@163.com>
2020-12-21 22:57:34 +0800

    跟新readme

--- Communication/new.py
+++ Communication/new.py
@@ -6,29 +6,29 @@
  @Describe:
 """
 import socket
-import RPi.GPIO as GPIO
-import time
-import signal
-import atexit
+# import RPi.GPIO as GPIO
+# import time
+# import signal
+# import atexit
 
-servopin = 18
-atexit.register(GPIO.cleanup)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servopin, GPIO.OUT, initial=False)
-p = GPIO.PWM(servopin, 50)
+# servopin = 18
+# atexit.register(GPIO.cleanup)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(servopin, GPIO.OUT, initial=False)
+# p = GPIO.PWM(servopin, 50)
 
 BUFSIZE = 1024
-ip_port = ('127.0.0.1', 9999)
+ip_port = ('192.168.1.198', 9999)
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # udp协议
 server.bind(ip_port)
 while True:
     data, client_addr = server.recvfrom(BUFSIZE)
     print('server收到的数据', data)
-    if data == '1':
-        p.start(0)
-        p.ChangeDutyCycle(2.5 + 10 * 90 / 180)
-        time.sleep(5)
-        p.ChangeDutyCycle(2.5)
+    # if data == '1':
+    #     p.start(0)
+    #     p.ChangeDutyCycle(2.5 + 10 * 90 / 180)
+    #     time.sleep(5)
+    #     p.ChangeDutyCycle(2.5)
     server.sendto(data.upper(), client_addr)
 
 server.close()
